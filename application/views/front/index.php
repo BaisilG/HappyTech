134594e3a2c0441b68401a20d90ad13cde4ef609
Baisil Geevarghese <pgbaisil@gmail.com>
2021-02-24 14:13:18 +0000

    Dashboard_

---
+++ application/views/front/index.php
@@ -0,0 +1,10 @@
+<!-- <?php require "section/header.php";?> -->
+<?php 
+    $this->load->view('front/pages/'.$page);
+?>
+<?php require "section/footer.php";?>
+
+
+<!-- //<?php require "section/footer.php";?> -->
+
+
---
+++ application/views/front/section/header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title><?php echo $page_title;?></title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <!-- <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/happy tech-touch-icon.png" rel="apple-touch-icon"> -->
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href=" assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+ 
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center">
+
+      <div class="logo mr-auto">
+        <h1 class="text-light"><a href="index.html"><span>Happy Tech</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="#index.html">Home</a></li>
+          <li><a href="#profile">Profile</a></li>
+          <li><a href="#application">Application</a></li>
+          <li><a href="#pending_reviews">Pending Reviews</a></li>
+          <li><a href="#reviewed">Reviewed</a></li>
+          
+          <li class="drop-down"><a href="">Login</a>
+            <ul>
+              <li><a href="#candidate">Candidate</a></li>
+              <li class="drop-down"><a href="#employee">Employee</a>
+</ul>
+                
+          </li>
+          
+
+         <!-- //<li class="get-started"><a href="#about">Login</a></li> -->
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
